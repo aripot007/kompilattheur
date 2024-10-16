7b6b3c69e212ab4bbb97b476424286b34500e703
Aristide Urli <aristide.urli@telecomnancy.eu>
2024-10-17 00:38:46 +0200

    feat: Use program arg for file path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod reader;
 mod lexer;
+use std::env;
 
 fn main() {
+
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file>", args[0]);
+        // Le programme final terminera ici, mais pour le dev je laisse un fichier par défaut
+        args.push(String::from("test_programs/hello_world.smolpp"));
+    }
     
-    let file_path = "test_programs/hello_world.smolpp".to_string();
+    let file_path = args[1].clone();
     let lexer = lexer::new(reader::new(file_path));
 
     for token in lexer {

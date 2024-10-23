9062269264e291f13f2dd4e05f7356166d71f2be
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2024-10-23 22:21:22 +0200

    feat(parser): change structure to an array of hashmap

--- src/parser/generate_tree.rs
+++ src/parser/generate_tree.rs
@@ -1,6 +1,9 @@
 use crate::lexer::lexer::Lexer;
-use crate::parser::analysis_table::AnalysisTable;
+use crate::parser::analysis_table::get_analysis_table;
 
-pub fn generate_tree(lexer: Lexer, analysis_table: AnalysisTable) {
-    // ...
+use super::lexem::{Lexem, NonTerminal};
+
+pub fn generate_tree(lexer: Lexer) {
+    let analysis_table = get_analysis_table();
+    let mut stack = Vec<Lexem>::New();
 }

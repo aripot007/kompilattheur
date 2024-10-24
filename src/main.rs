72fb81f597e430b5e1ea89ae75aa7a825b242fe2
Aristide Urli <aristide.urli@telecomnancy.eu>
2024-10-24 15:48:52 +0200

    feat: Use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,32 @@ mod lexer;
 mod parser;
 use lexer::lexer::Lexer;
 use parser::generate_tree::generate_tree;
-use std::env;
+use clap::Parser;
+
+/// MiniPython Compiler
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+
+    /// Génère une table d'analyse à partir du fichier d'entrée
+    #[arg(long="generate-parsing-table", action)]
+    generate_parsing_table: bool,
+
+    /// Le fichier à compiler
+    #[arg(default_value="test_programs/hello_world.smolpp")]
+    file: String,
+
+    /// Fichier de sortie
+    #[arg(short, long="output")]
+    output_file: Option<String>,
+
+}
 
 fn main() {
 
-    let mut args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        eprintln!("Usage: {} <file>", args[0]);
-        // Le programme final terminera ici, mais pour le dev je laisse un fichier par défaut
-        args.push(String::from("test_programs/hello_world.smolpp"));
-    }
-    
-    let file_path = args[1].clone();
+    let file_path = args.file;
     let lexer = Lexer::new(reader::new(file_path.clone()));
 
     for token in lexer {

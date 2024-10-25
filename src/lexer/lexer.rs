8b6fed3f339ea3447274626c03c69d458ee30c68
Aristide Urli <aristide.urli@telecomnancy.eu>
2024-10-25 11:42:40 +0200

    fix(token): Add missing "in" token

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -70,6 +70,7 @@ fn init_token_table() -> TokenTable {
     table.reserve_word("if", Token::If);
     table.reserve_word("else", Token::Else);
     table.reserve_word("for", Token::For);
+    table.reserve_word("in", Token::In);
     table.reserve_word("def", Token::Def);
     table.reserve_word("return", Token::Return);
     table.reserve_word("print", Token::Print);

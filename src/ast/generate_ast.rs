150dbf84d75daa8dd4ea5114f6c52a93b5d18e8a
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2024-10-29 13:02:44 +0100

    feat(ast): simplify node that had childs with only a terminal

--- src/ast/generate_ast.rs
+++ src/ast/generate_ast.rs
@@ -5,6 +5,20 @@ use crate::{common::types::Node, parser::Lexem};
 pub fn generate_ast(tree: Rc<RefCell<Node<Lexem>>>) {
     remove_empty_non_terminals(tree.clone()).unwrap_or(Node::new(Lexem::NonTerminal(0)));
     lift_single_child_nodes(tree.clone());
+    simplify_terminal_nodes(tree);
+}
+
+fn simplify_terminal_nodes(node: Rc<RefCell<Node<Lexem>>>) {
+    let children = node.borrow().get_children().clone();
+    if children.len() == 1 && !is_non_terminal(&children[0]) {
+        let terminal_value = children[0].borrow().value.clone();
+        node.borrow_mut().value = terminal_value;
+        node.borrow_mut().set_children(vec![]);
+    } else {
+        for child in children {
+            simplify_terminal_nodes(child);
+        }
+    }
 }
 
 fn remove_empty_non_terminals(node: Rc<RefCell<Node<Lexem>>>) -> Option<Rc<RefCell<Node<Lexem>>>> {

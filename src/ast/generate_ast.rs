e09664f084866f63d8a7ccbc95e7c9fd5a9158ab
PONSON--LISSALDE Romain <Romain.Ponson--Lissalde@telecomnancy.eu>
2024-11-06 17:05:03 +0100

    feat(ast): add functions remove_syntax_terminals is_syntax_terminal

--- src/ast/generate_ast.rs
+++ src/ast/generate_ast.rs
@@ -1,8 +1,19 @@
 use std::{cell::RefCell, rc::Rc};
 
-use crate::{common::types::Node, parser::Lexem};
+use crate::{
+    common::types::{Node, Token},
+    parser::Lexem,
+};
+
+/*
+TODO:
+Créer superset de Lexem pour AST
+Transitionner type de l'arbre
+Modifier les noeuds de l'arbre avec les nouveaux types
+*/
 
 pub fn generate_ast(tree: Rc<RefCell<Node<Lexem>>>) {
+    remove_syntax_terminals(tree.clone());
     remove_empty_non_terminals(tree.clone()).unwrap_or(Node::new(Lexem::NonTerminal(0)));
     lift_single_child_nodes(tree.clone());
     simplify_terminal_nodes(tree);
@@ -22,19 +33,19 @@ fn simplify_terminal_nodes(node: Rc<RefCell<Node<Lexem>>>) {
 }
 
 fn remove_empty_non_terminals(node: Rc<RefCell<Node<Lexem>>>) -> Option<Rc<RefCell<Node<Lexem>>>> {
-        if is_non_terminal(&node) && node.borrow().get_children().is_empty() {
-            return None;
-        }
-        let children = node.borrow().get_children().clone();
-        let mut new_children = vec![];
-        for child in children {
-            if let Some(non_empty_child) = remove_empty_non_terminals(child.clone()) {
-                new_children.push(non_empty_child);
-            }
-        }
-        node.borrow_mut().set_children(new_children);
-        Some(node)
+    if is_non_terminal(&node) && node.borrow().get_children().is_empty() {
+        return None;
     }
+    let children = node.borrow().get_children().clone();
+    let mut new_children = vec![];
+    for child in children {
+        if let Some(non_empty_child) = remove_empty_non_terminals(child.clone()) {
+            new_children.push(non_empty_child);
+        }
+    }
+    node.borrow_mut().set_children(new_children);
+    Some(node)
+}
 
 fn is_non_terminal(node: &Rc<RefCell<Node<Lexem>>>) -> bool {
     match node.borrow().value {
@@ -52,5 +63,39 @@ fn lift_single_child_nodes(node: Rc<RefCell<Node<Lexem>>>) {
     node.borrow_mut().set_children(children.clone());
     for child in children {
         lift_single_child_nodes(child);
+    }
+}
+
+fn remove_syntax_terminals(node: Rc<RefCell<Node<Lexem>>>) {
+    let children = node.borrow().get_children().clone();
+    let mut new_children = vec![];
+    for child in children {
+        if !is_syntax_terminal(child.clone()) {
+            new_children.push(child);
+        }
+    }
+    node.borrow_mut().set_children(new_children.clone());
+    for child in new_children {
+        remove_syntax_terminals(child);
+    }
+}
+
+fn is_syntax_terminal(node: Rc<RefCell<Node<Lexem>>>) -> bool {
+    match node.borrow().value {
+        Lexem::Terminal(ref t) => match t {
+            Token::Begin
+            | Token::CloseBracket
+            | Token::CloseParenthesis
+            | Token::Comma
+            | Token::Def
+            | Token::EOF
+            | Token::End
+            | Token::Newline
+            | Token::OpenBracket
+            | Token::OpenParenthesis
+            | Token::Sep => true,
+            _ => false,
+        },
+        _ => false,
     }
 }

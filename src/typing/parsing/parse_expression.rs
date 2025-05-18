18c1810b728f57a953b11c44348a4d0619eca47c
Aristide Urli <aristide.urli@telecomnancy.eu>
2025-05-18 14:25:38 +0200

    feat: Add weak types for simple arithmetic

--- src/typing/parsing/parse_expression.rs
+++ src/typing/parsing/parse_expression.rs
@@ -116,39 +116,41 @@ fn try_parse_binop(
         }
         BinOp::AND | BinOp::OR => Ok(Type::Bool),
         BinOp::LESS | BinOp::LESSEQ | BinOp::GREATER | BinOp::GREATEREQ => {
-            if t1 != t2 {
-                context.errors.push(Diagnostic::from_localizable(
-                    root_localization,
-                    DiagnosticGravity::Error,
-                    String::from("TypeError"),
-                    format!(
-                        "Cannot compare type {} with type {}",
-                        format!("{}", t1).color(Color::Yellow),
-                        format!("{}", t2).color(Color::Yellow)
-                    ),
-                ));
-                return Err(());
-            } else {
-                return Ok(Type::Bool);
-            }
+            try_parse_comparison(root_localization, e1, t1, op, e2, t2, context)
         }
         BinOp::MULT | BinOp::DIV | BinOp::MOD | BinOp::SUB => {
-            if t1 != Type::Int || t2 != Type::Int {
-                context.errors.push(Diagnostic::from_localizable(
-                    root_localization,
-                    DiagnosticGravity::Error,
-                    String::from("TypeError"),
-                    format!(
-                        "Incompatible types {} and {} for operand {}",
-                        format!("{}", t1).color(Color::Yellow),
-                        format!("{}", t2).color(Color::Yellow),
-                        format!("{}", op).color(Color::Magenta)
-                    ),
-                ));
-                return Err(());
-            } else {
-                return Ok(Type::Int);
+            match (&t1, &t2) {
+                (Type::Weak(w1), Type::Weak(w2)) => {
+                    w1.intersection(&w2);
+                    match w1.restrict(&[Type::Int]) {
+                        Ok(_) => return Ok(Type::Int),
+                        Err(_) => (),
+                    }
+                }
+                (t, Type::Weak(weak)) | (Type::Weak(weak), t) if t.is_compatible(Type::Int) => {
+                    match weak.restrict(&[Type::Int]) {
+                        Ok(_) => return Ok(Type::Int),
+                        Err(_) => (),
+                    }
+                }
+                (t1, t2) if t1.is_compatible(Type::Int) && t2.is_compatible(Type::Int) => {
+                    return Ok(Type::Int)
+                }
+                _ => (),
             }
+
+            context.errors.push(Diagnostic::from_localizable(
+                root_localization,
+                DiagnosticGravity::Error,
+                String::from("TypeError"),
+                format!(
+                    "Incompatible types {} and {} for operand {}",
+                    format!("{}", t1).color(Color::Yellow),
+                    format!("{}", t2).color(Color::Yellow),
+                    format!("{}", op).color(Color::Magenta)
+                ),
+            ));
+            return Err(());
         }
         BinOp::ADD if t1 != t2 => {
             context.errors.push(Diagnostic::from_localizable(
@@ -182,5 +184,32 @@ fn try_parse_binop(
                 Err(())
             }
         }
+    }
+}
+
+/// Parse comparison operators <, <=, > and >=
+fn try_parse_comparison(
+    root_localization: LocalizationInfo,
+    e1: &Expression,
+    e1_type: Type,
+    op: BinOp,
+    e2: &Expression,
+    e2_type: Type,
+    context: &mut TypingContext,
+) -> Result<Type, ()> {
+    if e1_type != e2_type {
+        context.errors.push(Diagnostic::from_localizable(
+            root_localization,
+            DiagnosticGravity::Error,
+            String::from("TypeError"),
+            format!(
+                "Cannot compare type {} with type {}",
+                format!("{}", e1_type).color(Color::Yellow),
+                format!("{}", e2_type).color(Color::Yellow)
+            ),
+        ));
+        return Err(());
+    } else {
+        return Ok(Type::Bool);
     }
 }

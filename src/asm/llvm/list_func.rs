011df9fdcf80f40a284ad75b5eb69b9fd6dc1092
Luca Mandrelli <luca.mandrelli@telecomnancy.eu>
2025-05-18 17:53:33 +0200

    feat(range, list) : basic definition, without core logic

--- src/asm/llvm/list_func.rs
+++ src/asm/llvm/list_func.rs
@@ -1,13 +1,47 @@
-use crate::asm::codegen::CodeGen;
+use crate::{
+    asm::{codegen::CodeGen, InternalFuctions},
+    typing::Type,
+};
 
-use super::LLVMCodegenError;
+use super::{assert_type, LLVMCodegenError};
 
+// Initialize the internal "list" function that will give a list for range(n) or a list
 pub fn init_internal_list_function<'ctx>(cg: &mut CodeGen<'ctx>) -> Result<(), LLVMCodegenError> {
+    let void_type = cg.context.void_type();
+    let param_type = cg.smolpp_types.dynamic_type;
+
+    // register function in module
+    let func_type = void_type.fn_type(&[param_type.into()], false);
+    let function = cg
+        .module
+        .add_function(InternalFuctions::List.into(), func_type, None);
+
+    // build function
+    let entry = cg.context.append_basic_block(function, "list_entry");
+
+    // switch builder to function block
+    cg.builder.position_at_end(entry);
+    cg.current_function = function;
+
+    let param = function.get_first_param().unwrap().into_struct_value();
+
+    // Assert that the type is a list
+    assert_type(
+        Type::List,
+        &param,
+        cg,
+        Some("Expected list type for list parameter".to_string()),
+    )?;
+
     // TODO: implement list()
     // assert type before exec
     // list(range(n)) -> [0, 1, ... n-1],
     // list([4, 5]) -> [4, 5],
     // list(non list) -> error
 
-    Ok(())
+    // Return builder to main block
+    cg.current_function = cg.main_function;
+    cg.builder.position_at_end(cg.current_main_block);
+
+    return Ok(());
 }
--- src/asm/llvm/range.rs
+++ src/asm/llvm/range.rs
@@ -1,10 +1,44 @@
-use crate::asm::codegen::CodeGen;
+use crate::{
+    asm::{codegen::CodeGen, InternalFuctions},
+    typing::Type,
+};
 
-use super::LLVMCodegenError;
+use super::{assert_dyn_type, assert_type, LLVMCodegenError};
 
+//Initialize the internal "range" function that will generate a list of numbers from 0 to n-1, given n
 pub fn init_internal_range_function<'ctx>(cg: &mut CodeGen<'ctx>) -> Result<(), LLVMCodegenError> {
-    // TODO: implement range()
-    // range(n) -> [0, 1, ... n-1]
+    let void_type = cg.context.void_type();
+    let param_type = cg.smolpp_types.dynamic_type;
 
-    Ok(())
+    // register function in module
+    let func_type = void_type.fn_type(&[param_type.into()], false);
+    let function = cg
+        .module
+        .add_function(InternalFuctions::Range.into(), func_type, None);
+
+    // build function
+    let entry = cg.context.append_basic_block(function, "range_entry");
+
+    // switch builder to function block
+    cg.builder.position_at_end(entry);
+    cg.current_function = function;
+
+    let param = function.get_first_param().unwrap().into_struct_value();
+
+    // Assert that the type is an integer
+    assert_type(
+        Type::Int,
+        &param,
+        cg,
+        Some("Expected i32 type for range parameter".to_string()),
+    )?;
+
+    // TODO: implement range()
+    // returns a list of numbers from 0 to n-1 for given n
+
+    // Return builder to main block
+    cg.current_function = cg.main_function;
+    cg.builder.position_at_end(cg.current_main_block);
+
+    return Ok(());
 }

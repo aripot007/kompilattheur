73070ccdcb7cea1846373e2abedec732ed9cf3bf
Luca Mandrelli <luca.mandrelli@telecomnancy.eu>
2025-05-18 19:06:46 +0200

    feat(range) : implemented internal range function, not working

--- src/asm/llvm/range.rs
+++ src/asm/llvm/range.rs
@@ -3,9 +3,10 @@ use crate::{
     typing::Type,
 };
 
-use super::{assert_dyn_type, assert_type, LLVMCodegenError};
+use super::{assert_type, LLVMCodegenError};
+use inkwell::IntPredicate;
 
-//Initialize the internal "range" function that will generate a list of numbers from 0 to n-1, given n
+// Initialize the internal "range" function that will generate a list of numbers from 0 to n-1
 pub fn init_internal_range_function<'ctx>(cg: &mut CodeGen<'ctx>) -> Result<(), LLVMCodegenError> {
     let void_type = cg.context.void_type();
     let param_type = cg.smolpp_types.dynamic_type;
@@ -18,6 +19,9 @@ pub fn init_internal_range_function<'ctx>(cg: &mut CodeGen<'ctx>) -> Result<(), 
 
     // build function
     let entry = cg.context.append_basic_block(function, "range_entry");
+    let loop_check = cg.context.append_basic_block(function, "range_check");
+    let loop_body = cg.context.append_basic_block(function, "range_body");
+    let loop_end = cg.context.append_basic_block(function, "range_end");
 
     // switch builder to function block
     cg.builder.position_at_end(entry);
@@ -33,12 +37,67 @@ pub fn init_internal_range_function<'ctx>(cg: &mut CodeGen<'ctx>) -> Result<(), 
         Some("Expected i32 type for range parameter".to_string()),
     )?;
 
-    // TODO: implement range()
-    // returns a list of numbers from 0 to n-1 for given n
+    let upper_bound = param.get_field_at_index(0).unwrap().into_int_value();
+    let (list_var, list_ptr) = cg.build_list_variable(upper_bound, false)?;
 
-    // Return builder to main block
+    // initialize loop counter
+    let i_ptr = cg.builder.build_alloca(cg.context.i64_type(), "i_ptr")?;
+    cg.builder
+        .build_store(i_ptr, cg.context.i64_type().const_zero())?;
+    cg.builder.build_unconditional_branch(loop_check)?;
+
+    // check
+    cg.builder.position_at_end(loop_check);
+    let i_val = cg
+        .builder
+        .build_load(cg.context.i64_type(), i_ptr, "i_val")?
+        .into_int_value();
+    let cond = cg
+        .builder
+        .build_int_compare(IntPredicate::ULT, i_val, upper_bound, "range_cond")?;
+    cg.builder
+        .build_conditional_branch(cond, loop_body, loop_end)?;
+
+    // body
+    cg.builder.position_at_end(loop_body);
+    let list_struct = cg
+        .builder
+        .build_load(cg.smolpp_types.list_type, list_ptr, "list_struct")?
+        .into_struct_value();
+    let array_ptr = cg.build_get_list_array_ptr(list_struct)?;
+    let elem_ptr = unsafe {
+        cg.builder.build_gep(
+            cg.smolpp_types.dynamic_type,
+            array_ptr,
+            &[i_val.into()],
+            "elem_ptr",
+        )
+    }?;
+    let elem_var = cg.create_variable(Type::Int, i_val)?;
+    cg.builder.build_store(elem_ptr, elem_var)?;
+
+    // update length
+    let len = cg.build_get_list_length(list_struct)?;
+    let new_len =
+        cg.builder
+            .build_int_add(len, cg.context.i64_type().const_int(1, false), "inc_len")?;
+    let updated = cg
+        .builder
+        .build_insert_value(list_struct, new_len, 0, "set_list_len")?
+        .into_struct_value();
+    cg.builder.build_store(list_ptr, updated)?;
+    // increment i
+    let one = cg.context.i64_type().const_int(1, false);
+    let next_i = cg.builder.build_int_add(i_val, one, "inc_i")?;
+    cg.builder.build_store(i_ptr, next_i)?;
+    cg.builder.build_unconditional_branch(loop_check)?;
+
+    // end
+    cg.builder.position_at_end(loop_end);
+    cg.builder.build_return(None)?;
+
+    // restore
     cg.current_function = cg.main_function;
     cg.builder.position_at_end(cg.current_main_block);
-
-    return Ok(());
+    Ok(())
 }
